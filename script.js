41fd3d7ba703538c90f6fb59e901432552b17532
SwegCet <ethantruong0804@gmail.com>
2024-06-29 01:42:53 -0700

    Finished Working on the AddBookToLibrary() function, need to work on removing

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const myLibrary = []; //Empty Library Array
 
-
 // This is the constructor
-function Book(name, author, pages, read) {
-    this.name = name;
+function Book(title, author, pages, read) {
+    this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
@@ -15,14 +14,15 @@ function addBookToLibrary() {
     const title = document.querySelector('.bookTitleInput').value;
     const author = document.querySelector('.bookAuthorInput').value;
     const pages = document.querySelector('.bookPagesInput').value;
-    const check = document.querySelector('.bookReadInput').checked;
+    const read = document.querySelector('.bookReadInput').checked;
     //Check if the inputs were filled
     if (title && author && pages) {
         //If everything is filled, populate the array
-        newBook = new Book(name, author, pages, read);
+        const newBook = new Book(title, author, pages, read);
         myLibrary.push(newBook);
         displayBook();
         //Clear form for user
+        const bookForm = document.querySelector("#bookForm")
         bookForm.reset()
     }
     else {
@@ -30,11 +30,55 @@ function addBookToLibrary() {
     }
     //Need a Display function to display the new book
     function displayBook() {
+        //Select the Display Div so that it displays the cards there
+        const display = document.querySelector('.display');
+
+        //Clear previous content
+        display.innerHTML = '';
+
         //Loops through the array to display each book
         myLibrary.forEach((newBook, index) => {
+            //Need to create new elements to display the userInput cards
+            //Populate these "new" elements with the userInput (Objects)
+            // Create Card
+            const card = document.createElement("div");
+            card.className = 'card';
 
+            const titleDisplay = document.createElement("div");
+            titleDisplay.className = 'title';
+            titleDisplay.textContent = newBook.title;
+
+            const authorDisplay = document.createElement("div");
+            authorDisplay.className = 'author';
+            authorDisplay.textContent = newBook.author;
+
+            const pagesDisplay = document.createElement("div");
+            pagesDisplay.className = 'pages';
+            pagesDisplay.textContent = `${newBook.pages} Pages`;
+
+            const readContainerDisplay = document.createElement("div");
+            readContainerDisplay.className = 'read-container';
+
+            const labelDisplay = document.createElement("label");
+            labelDisplay.setAttribute('for', `read-${index}`)
+            labelDisplay.textContent = "Read: ";
+
+            const readDisplay = document.createElement("input")
+            readDisplay.type = "checkbox";
+            readDisplay.className = 'read';
+            readDisplay.name = `read-${index}`;
+            readDisplay.checked = newBook.read;
+
+            //Last thing to do is appendChild into card
+            readContainerDisplay.appendChild(labelDisplay);
+            readContainerDisplay.appendChild(readDisplay);
+
+            card.appendChild(titleDisplay);
+            card.appendChild(authorDisplay);
+            card.appendChild(pagesDisplay);
+            card.appendChild(readContainerDisplay);
+
+            display.appendChild(card);
         });
-
-        //Last thing to do is appendChild into card
     }
 }
